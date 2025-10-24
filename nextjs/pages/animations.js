07236bff24fdb743a8630c53881f847834f91563
joho <73869409+johostudio@users.noreply.github.com>
2025-10-23 17:34:09 -0700

    tailwind, tbd page, next.js

---
+++ nextjs/pages/animations.js
@@ -0,0 +1,11 @@
+// Minimal Next.js page scaffold you can use inside a Next project.
+// Move this file into a Next.js project at /pages/animations.js to use.
+import React from 'react';
+
+export default function Animations() {
+  return (
+    <div style={{background:'#000',height:'100vh',display:'flex',alignItems:'center',justifyContent:'center'}}>
+      <h1 style={{color:'#fff',fontFamily:'"Helvetica Neue", Helvetica, Arial, sans-serif'}}>tbd</h1>
+    </div>
+  );
+}
